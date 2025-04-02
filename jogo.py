822e715aa9be07d1c623362cb6c802c8b373eb2d
juliamf791 <julia.mf8@puccampinas.edu.br>
2025-04-02 11:24:16 -0300

    adicionado interação com o usuario
    add a biblio, a var do codigo e do palpite

--- jogo.py
+++ jogo.py
@@ -1,5 +1,11 @@
-print("BEM VINDO AO JOGO SECRETO!: ")
+print("BEM VINDO AO JOGO SECRETO!! ")
+
+
 print("O objetivo é acertar o número secreto. ")
 print("Você 10 tentativas para acertar o número secreto entre [1000 e 9999]")
-print("A parTIR da 5a. tentativa o jogo irá te ajudar , dando dicas ")
+print("A partir da 5° tentativa o jogo irá te ajudar com  dicas ")
 print("\n>>>TECLE ALGO PARA CONTINUAR <<<<: ")
+
+import random 
+codig_secret= random.randint(1000,9999)
+palpitec=int(input("digite um número com 4 dígitos:"))
