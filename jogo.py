4608c36de26eee007dfbd91b570c90b40931c7a1
heloisa <125582181+loisaaz@users.noreply.github.com>
2025-04-02 11:36:21 -0300

    Organizei o código
    adicionado "\n" ao prints

--- jogo.py
+++ jogo.py
@@ -1,11 +1,9 @@
-print("BEM VINDO AO JOGO SECRETO!! ")
-
-
-print("O objetivo é acertar o número secreto. ")
-print("Você 10 tentativas para acertar o número secreto entre [1000 e 9999]")
-print("A partir da 5° tentativa o jogo irá te ajudar com  dicas ")
-print("\n>>>TECLE ALGO PARA CONTINUAR <<<<: ")
+print("\nBEM VINDO AO JOGO SECRETO!!")
+print("\nO objetivo é acertar o número secreto.")
+print("\nVocê 10 tentativas para acertar o número secreto entre [1000 e 9999]")
+print("\nA partir da 5° tentativa o jogo irá te ajudar com  dicas")
+print("\n>>>TECLE ALGO PARA CONTINUAR <<<<:")
 
 import random 
 codig_secret= random.randint(1000,9999)
-palpitec=int(input("digite um número com 4 dígitos:"))
+palpitec=int(input("Digite um número com 4 dígitos:"))
