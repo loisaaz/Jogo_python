3b3eed1d5bdc576989ef46e4b9a910647c507dca
heloisa <125582181+loisaaz@users.noreply.github.com>
2025-04-02 19:20:23 -0300

    adicionado input() tecle algo.
    o input() serve para quando o usuário apertar a tecla ENTER, por exemplo, a próxima parte do código apareça, no caso o input para digitar o palpite.

--- jogo.py
+++ jogo.py
@@ -3,7 +3,9 @@ print("\nO objetivo é acertar o número secreto.")
 print("\nVocê 10 tentativas para acertar o número secreto entre [1000 e 9999]")
 print("\nA partir da 5° tentativa o jogo irá te ajudar com  dicas")
 print("\n>>>TECLE ALGO PARA CONTINUAR <<<<:")
+input() # tecle algo para aparecer a próxima tela
 
 import random 
 codig_secret= random.randint(1000,9999)
 palpitec=int(input("Digite um número com 4 dígitos:"))
+
