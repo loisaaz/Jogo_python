10da7206cff4c9208a241512967537f5adcd9a1d
heloisa <125582181+loisaaz@users.noreply.github.com>
2025-04-03 12:09:38 -0300

    comentado os prints
    print para teste se a divisão estava funcionando foram comentados

--- jogo.py
+++ jogo.py
@@ -14,10 +14,10 @@ digito_1 = palpite // 1000
 digito_2 = (palpite // 100) % 10
 digito_3 = (palpite // 10) % 10  
 digito_4 = palpite % 10 
-print(digito_1)
-print(digito_2)
-print(digito_3)
-print(digito_4)
+#print(digito_1)
+#print(digito_2)
+#print(digito_3)
+#print(digito_4)
 
 
 # separar digitos codigo secreto
@@ -25,11 +25,10 @@ cod_secreto_1 = codig_secret // 1000
 cod_secreto_2 = (codig_secret // 100) % 10
 cod_secreto_3 = (codig_secret // 10) % 10  
 cod_secreto_4 = codig_secret % 10 
-
-print(cod_secreto_1)
-print(cod_secreto_2)
-print(cod_secreto_3)
-print(cod_secreto_4)
+#print(cod_secreto_1)
+#print(cod_secreto_2)
+#print(cod_secreto_3)
+#print(cod_secreto_4)
 
 # adicionar um loop para comparaçao entre o palpite e o cod secreto (for ou while)
 # criar variáveis para contar (logo entendemos que precisamos adicionar um contador) quantos dígitos estão corretos (se correto deve aparecer) e quantos estão na posição certa.
