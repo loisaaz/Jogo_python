198d58f8dab6a0a56da004274fb3140211c02941
heloisa <125582181+loisaaz@users.noreply.github.com>
2025-04-03 19:31:13 -0300

    mudanças! na descrição
    adicionei:
    if, elif, in lista, contador e alguns prints.
    ainda falta adicionar um acumulador de qual numero o usuario acertou assim como alguns prints (dicas e telas de parabens ou reiniciar o game).

--- jogo.py
+++ jogo.py
@@ -8,41 +8,77 @@ input() # tecle algo para aparecer a próxima tela
 import random 
 codig_secret= random.randint(1000,9999) #biblioteca
 
-tentativas = 0
+tentativas = 0 # loop para contagem de tentativas
 max_tentativas = 10
+
 while tentativas < max_tentativas:
-    palpite=int(input("Digite seu palpite sendo um número de 4 dígitos:"))
+    palpite = int(input("Digite seu palpite sendo um número de 4 dígitos:"))
+    if palpite < 1000 or palpite > 9999:
+        print("\nATENÇÃO! ! !")
+        print("\nINSIRA UM VALOR NO INTERVALO DE 1000 E 9999")
+        continue
 
-# separar dígitos do palpite
-digito_1 = palpite // 1000     
-digito_2 = (palpite // 100) % 10
-digito_3 = (palpite // 10) % 10  
-digito_4 = palpite % 10 
-#print(digito_1)
-#print(digito_2)
-#print(digito_3)
-#print(digito_4)
+    tentativas +=1
+    tentativas_restantes = max_tentativas - tentativas
+    print(f"\nTentativas restantes {tentativas_restantes}")
 
-# separar digitos codigo secreto
-cod_secreto_1 = codig_secret // 1000     
-cod_secreto_2 = (codig_secret // 100) % 10
-cod_secreto_3 = (codig_secret // 10) % 10  
-cod_secreto_4 = codig_secret % 10 
-#print(cod_secreto_1)
-#print(cod_secreto_2)
-#print(cod_secreto_3)
-#print(cod_secreto_4)
+    # separar dígitos do palpite
+    digito_1 = palpite // 1000     
+    digito_2 = (palpite // 100) % 10
+    digito_3 = (palpite // 10) % 10  
+    digito_4 = palpite % 10 
+    #print(digito_1)
+    #print(digito_2)
+    #print(digito_3)
+    #print(digito_4)
 
-# adicionar um loop para comparaçao entre o palpite e o cod secreto (for ou while)
-# criar variáveis para contar (logo entendemos que precisamos adicionar um contador) quantos dígitos estão corretos (se correto deve aparecer) e quantos estão na posição certa.
-# loop para contagem de tentativas
+    # separar digitos codigo secreto
+    cod_secreto_1 = codig_secret // 1000     
+    cod_secreto_2 = (codig_secret // 100) % 10
+    cod_secreto_3 = (codig_secret // 10) % 10  
+    cod_secreto_4 = codig_secret % 10 
+    #print(cod_secreto_1)
+    #print(cod_secreto_2)
+    #print(cod_secreto_3)
+    #print(cod_secreto_4)
 
-#variáveis para contar dígitos corretos na posição certa e na posição errada.
-corretos_certos = 0
-corretos_errados = 0
+    #variáveis para contar dígitos corretos/errados.
+    digitos_certos = 0
+    digitos_errados = 0
 
-if digito_1 == cod_secreto_1:
-    corretos_certos +=1
-else:
-    digito_1 == cod_secreto_2 or cod_secreto_3 or cod_secreto_4
-    corretos_errados += 1
+# escolhi usar elif porque estamos trabalhando com bastantes possibilidades e ele facilita mais que o else.
+# as listas tornam o trabalho mais fácil caso algo precise ser alterado e o código fica mais limpo 
+# especialmente quando há múltiplas verificações. 
+
+    if digito_1 == cod_secreto_1:
+        digitos_certos += 1
+        print("\nVocê acertou 1 dígito(s)!")
+    elif digito_1 in [cod_secreto_2, cod_secreto_3, cod_secreto_4]:
+        digitos_errados += 1
+        print("\nVocê não acertou nenhum dígito.")
+
+    if digito_2 == cod_secreto_2:
+        digitos_certos += 1
+        print("\nVocê acertou 1 dígito(s)!")
+    elif digito_2 in [cod_secreto_1, cod_secreto_3, cod_secreto_4]:
+        digitos_errados += 1
+        print("\nVocê não acertou nenhum dígito.")
+
+    if digito_3 == cod_secreto_3:
+        digitos_certos += 1
+        print("\nVocê acertou 1 dígito(s)!")
+    elif digito_3 in [cod_secreto_1, cod_secreto_2, cod_secreto_4]:
+        digitos_errados += 1
+        print("\nVocê não acertou nenhum dígito.")
+
+    if digito_4 == cod_secreto_4:
+        digitos_certos += 1
+        print("\nVocê acertou 1 dígito(s)!")
+    elif digito_4 in [cod_secreto_1, cod_secreto_2, cod_secreto_3]:
+        digitos_errados += 1
+        print("\nVocê não acertou nenhum dígito.")
+
+    if tentativas == max_tentativas:
+        print(f"\nVocê não acertou o número secreto. O número era {codig_secret}.")
+  
+# print("\nSeu dígito é:")
