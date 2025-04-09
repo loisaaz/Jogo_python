a2499aa626cf8f1d53d1dbfa02e4a67ed42149d6
heloisa <125582181+loisaaz@users.noreply.github.com>
2025-04-09 12:22:15 -0300

    O ARQUIVO 'JOGOANTIGO.PY' É O CÓDIGO QUE USAMOS DE BASE PARA RESPONDER AS PERGUNTAS DA LUCIA
    CONTINUAREMOS REALIZANDO MUDANÇAS NESSE ARQUIVO.
    O que ainda precisamos mudar está no whatsapp

--- jogoantigo.py
+++ jogoantigo.py
@@ -1,30 +1,34 @@
 print("\nBEM VINDO AO JOGO SECRETO!!")
 print("\nO objetivo é acertar o número secreto.")
-print("\nVocê tem 10 tentativas para acertar o número secreto entre [1000 e 9999]")
-print("\nA partir da 5ª tentativa, o jogo irá te ajudar com dicas.")
-print("\n>>> TECLE ALGO PARA CONTINUAR <<<:")
+print("\nVocê 10 tentativas para acertar o número secreto entre [1000 e 9999]")
+print("\nA partir da 5° tentativa o jogo irá te ajudar com  dicas")
+print("\n>>>TECLE ALGO PARA CONTINUAR <<<<:")
 input()
 
 import random 
-codig_secret = random.randint(1000, 9999) 
+codig_secret= random.randint(1000,9999) 
 
 tentativas = 0 
 max_tentativas = 10
-acertos = 0
-prog_digito = ["_", "_", "_", "_"] 
+#acertos = 0
+#posicao = 0
+progresso = "_ _ _ _"
+prog_digito_1 = "_"
+prog_digito_2 = "_"
+prog_digito_3 = "_"
+prog_digito_4 = "_"
 
 while tentativas < max_tentativas:
     print("\n---------------------------------------")
-    palpite = int(input("Digite seu palpite sendo um número de 4 dígitos: "))
-    
+    palpite = int(input("Digite seu palpite sendo um número de 4 dígitos:"))
     if palpite < 1000 or palpite > 9999:
         print("\nATENÇÃO! ! !")
         print("\nINSIRA UM VALOR NO INTERVALO DE 1000 - 9999 E INTEIRO")
-        break
+        continue
 
-    tentativas += 1
+    tentativas +=1
     tentativas_restantes = max_tentativas - tentativas
-    print(f"\nTentativas restantes: {tentativas_restantes}")
+    print(f"\nTentativas restantes {tentativas_restantes}")
     print("\n---------------------------------------")
 
     digito_1 = palpite // 1000     
@@ -72,24 +76,31 @@ while tentativas < max_tentativas:
     #print("\nNúmeros corretos na posição certa:", posicao)
     #print("\nNúmeros corretos na posição errada:", acertos - posicao)
 
-    print(f"\nSeu código é: {prog_digito_1} {prog_digito_2} {prog_digito_3} {prog_digito_4}")
     if prog_digito_1 or prog_digito_2 or prog_digito_3 or prog_digito_4 == codig_secret:
+        print(f"\nSeu código é: {prog_digito_1} {prog_digito_2} {prog_digito_3} {prog_digito_4}")
         print("\nVocê acertou 1 dígito(s)!")
     else:
         print("\nVocê não acertou nenhum dígito.")
-
+           
+# Dicas a partir da 5ª tentativa
     if tentativas >= 5:
-        if palpite < codig_secret:
-            print("DICA: O número secreto é MAIOR que seu palpite.")
-        else:
-            print("DICA: O número secreto é MENOR que seu palpite.")
- 
-    if palpite == codig_secret:
-            print("\nPARABÉNS! Você acertou o número secreto!")
-            break
-    
-    if tentativas == max_tentativas:
-            print(f"\nVocê não acertou o número secreto. O número era {codig_secret}.")
-            print("Obrigado por jogar!")
-            jogar_novamente = int(input("\nDeseja jogar novamente? (1 - Sim / 0 - Não): "))
-            
+        if prog_digito_1 == -1:
+            if digito_1 >= 5:
+                print("\n(DICA: O número na posição 1 é maior ou igual a 5)")
+            else:
+                print("\n(DICA: O número na posição 1 é menor que 5)")
+        elif prog_digito_2 == -1:
+            if digito_2 >= 5:
+                print("\n(DICA: O número na posição 2 é maior ou igual a 5)")
+            else:
+                print("\n(DICA: O número na posição 2 é menor que 5)")
+        elif prog_digito_3 == -1:
+            if digito_3 >= 5:
+                print("\n(DICA: O número na posição 3 é maior ou igual a 5)")
+            else:
+                print("\n(DICA: O número na posição 3 é menor que 5)")
+        elif prog_digito_4 == -1:
+            if digito_4 >= 5:
+                print("\n(DICA: O número na posição 4 é maior ou igual a 5)")
+            else:
+                print("\n(DICA: O número na posição 4 é menor que 5)")
