d454e46dcf20f4b68d0927a3a750d89bff6e33e4
heloisa <125582181+loisaaz@users.noreply.github.com>
2025-04-10 09:57:08 -0300

    reajustado os prints de digitos certos e errados.

--- jogoantigo.py
+++ jogoantigo.py
@@ -11,8 +11,6 @@ codig_secret= random.randint(1000,9999)
 tentativas = 0 
 max_tentativas = 10
 
-
-progresso = "_ _ _ _"
 prog_digito_1 = "_"
 prog_digito_2 = "_"
 prog_digito_3 = "_"
@@ -43,43 +41,43 @@ while tentativas < max_tentativas:
 
     if digito_1 == cod_secreto_1:
         prog_digito_1 = digito_1
-        print("\nVocê acertou 1 dígito(s)!")
+        print("\nVocê acertou o dígito da casa de milhares!")
 
     else: 
         digito_1 != cod_secreto_1
-        print("\nVocê não acertou nenhum dígito.")
+        print("\nVocê não acertou o dígito da casa de milhares.")
 
     if digito_2 == cod_secreto_2:
         prog_digito_2 = digito_2
-        print("\nVocê acertou 1 dígito(s)!")
+        print("\nVocê acertou o dígito da casa das centenas!")
 
     else:
         digito_2 != cod_secreto_2
-        print("\nVocê não acertou nenhum dígito.")
+        print("\nVocê não acertou o dígito da casa das centenas.")
 
     if digito_3 == cod_secreto_3:
         prog_digito_3 = digito_3
-        print("\nVocê acertou 1 dígito(s)!")
+        print("\nVocê acertou o dígito da casa das dezenas!")
 
     else:
         digito_3 != cod_secreto_3
-        print("\nVocê não acertou nenhum dígito.")
+        print("\nVocê não acertou o dígito da casa das dezenas.")
 
     if digito_4 == cod_secreto_4:
         prog_digito_4 = digito_4
-        print("\nVocê acertou 1 dígito(s)!")
+        print("\nVocê acertou o dígito da casa das unidades!")
 
     else:
         digito_4 != cod_secreto_4
-        print("\nVocê não acertou nenhum dígito.")
-
-    #print("\nNúmeros corretos na posição certa:", posicao)
-    #print("\nNúmeros corretos na posição errada:", acertos - posicao)
+        print("\nVocê não acertou o dígito da casa das unidades.")
+    #if digito_1 == cod_secreto_1 or digito_2 == cod_secreto_2 or digito_3 == cod_secreto_3 or digito_4 == cod_secreto_4:
+        #print(f"\nVocê acertou 1 dígito(s)!")
+    #else:
+        #print("\nVocê não acertou nenhum dígito.")
 
     if prog_digito_1 or prog_digito_2 or prog_digito_3 or prog_digito_4 == codig_secret:
-        print(f"\nSeu código é: {prog_digito_1} {prog_digito_2} {prog_digito_3} {prog_digito_4}")
-           
-# Dicas a partir da 5ª tentativa
+        print(f"\nSeu código é: {prog_digito_1} {prog_digito_2} {prog_digito_3} {prog_digito_4}")           
+
     if tentativas >= 5:
         if prog_digito_1 == -1:
             if digito_1 >= 5:
@@ -96,8 +94,13 @@ while tentativas < max_tentativas:
                 print("\n(DICA: O número na posição 3 é maior ou igual a 5)")
             else:
                 print("\n(DICA: O número na posição 3 é menor que 5)")
-        elif prog_digito_4 == -1:
-            if digito_4 >= 5:
-                print("\n(DICA: O número na posição 4 é maior ou igual a 5)")
-            else:
-                print("\n(DICA: O número na posição 4 é menor que 5)")
+        
+        if max_tentativas > tentativas:
+            print("\n---------------------------------------")
+            print(f"\nVOCÊ NÃO ACERTOU O NÚMERO SECRETO, O CÓDIGO ERA: {codig_secret}") 
+            jogar_novamente = int(input("\nDeseja jogar novamente? (1 - Sim / 0 - Não): "))
+
+        if palpite == codig_secret:
+            print("P A R A B É N S ! ! !")
+            print("VOCÊ ACERTOU O CÓDIGO SECRETO ! ! !")
+   
